704c14447ef81eec1646a0e8d82cf5f12d8837c4
pajic <32738446+pajicadvance@users.noreply.github.com>
2025-08-22 14:27:20 +0200

    Fix creeper explosions not respecting mobGriefing gamerule

--- src/main/java/me/pajic/misctweaks/mixin/CreeperMixin.java
+++ src/main/java/me/pajic/misctweaks/mixin/CreeperMixin.java
@@ -1,14 +1,20 @@
 package me.pajic.misctweaks.mixin;
 
 import me.pajic.misctweaks.config.ModServerConfig;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(Creeper.class)
-public class CreeperMixin {
+public abstract class CreeperMixin extends Monster {
+    protected CreeperMixin(EntityType<? extends Monster> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
 
     @ModifyArg(
             method = "explodeCreeper",
@@ -19,6 +25,7 @@ public class CreeperMixin {
             index = 5
     )
     private Level.ExplosionInteraction creeperExplosionDropsAll(Level.ExplosionInteraction original) {
-        return ModServerConfig.creeperExplosionDropsAllItems ? Level.ExplosionInteraction.TNT : original;
+        return ModServerConfig.creeperExplosionDropsAllItems && level().getGameRules().getBoolean(GameRules.RULE_MOBGRIEFING)
+                ? Level.ExplosionInteraction.TNT : original;
     }
 }
